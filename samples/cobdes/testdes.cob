54b93d9764f1dcff6182937b9737de0791ce8f19
Simon Sobisch <simonsobisch@gnu.org>
2014-09-26 19:24:13 +0000

    license typo fixed

--- samples/cobdes/testdes.cob
+++ samples/cobdes/testdes.cob
@@ -16,10 +16,10 @@
 *>**********************************************************************
 
 *>**********************************************************************
-*>  TESTDES compiler is free software: you can redistribute it and/or
-*>  modify it under the terms of the GNU General Public License as
-*>  published by the Free Software Foundation, either version 3 of
-*>  the License, or (at your option) any later version.
+*>  TESTDES is free software: you can redistribute it and/or modify it
+*>  under the terms of the GNU General Public License as published by
+*>  the Free Software Foundation, either version 3 of the License, or
+*>  (at your option) any later version.
 *>
 *>  TESTDES is distributed in the hope that it will be useful, but
 *>  WITHOUT ANY WARRANTY; without even the implied warranty of
