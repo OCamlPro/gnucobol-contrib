f15668c3160b00e66698a9b047252622c9f16e11
Simon Sobisch <simonsobisch@gnu.org>
2017-01-03 10:17:02 +0000

    Version bump to "2.0.0 dev" Set standard width to 1200 pixel
    Move paths.ini to paths.tmpl.ini and ignore paths.ini (as it contains local configuration)
    
    Changes in Windows-Batches:
    * check if needed executables exists before running these
    * wait at end if not started from command prompt
    
    BPEdit.bat:
    * change the working directory before starting
    * check if paths.ini exists, if not create from template

--- tools/BPEdit/BPEdit.bat
+++ tools/BPEdit/BPEdit.bat
@@ -1,1 +1,40 @@
-python BPEdit.pyw
+@echo off
+setlocal
+
+:: check if started directly
+echo %cmdcmdline% | find /i "%~0" >nul
+if errorlevel 1 (
+   set interactive=1
+) else (
+   set interactive=0
+)
+
+:: check for python executable
+where /q python.exe
+if errorlevel 1 (
+   echo ERROR: python.exe is missing in PATH
+   goto :end
+)
+
+:: change to project directory
+pushd %~dp0
+
+:: create paths.ini from template, if missing
+if not exist "paths.ini" (
+   echo path.ini is created from template, please adjust the paths
+   copy "paths.tmpl.ini" "paths.ini"
+   echo.
+   pause
+   echo.
+)
+
+:: actual start
+python.exe BPEdit.pyw
+
+:end
+if _%interactive%_==_0_ (
+   echo.
+   pause
+)
+
+endlocal
--- tools/BPEdit/env-setup.bat
+++ tools/BPEdit/env-setup.bat
@@ -1,1 +1,41 @@
- pip install -r requirements.txt
+@echo off
+setlocal
+
+:: check if started directly
+echo %cmdcmdline% | find /i "%~0" >nul
+if errorlevel 1 (
+   set interactive=1
+) else (
+   set interactive=0
+)
+
+:: check for pip executable
+where /q pip3.exe
+if errorlevel 1 (
+   where /q pip.exe
+   if errorlevel 0 (
+      set PIP=pip.exe
+   )
+) else (
+   set PIP=pip3.exe
+)
+if errorlevel 1 (
+   echo ERROR: pip3.exe / pip.exe is missing in PATH
+   goto :end
+)
+
+:: actual install
+echo Installing Dependencies...
+echo %PIP% install -r requirements.txt
+
+echo.
+%PIP% install -r requirements.txt
+
+
+:end
+if _%interactive%_==_0_ (
+   echo.
+   pause
+)
+
+endlocal
