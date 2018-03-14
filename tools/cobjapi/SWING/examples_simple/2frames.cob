e6e5df558087a64a02661901ac3be0f9d585a2e3
László Erdős <laszloerdos@users.sourceforge.net>
2018-03-14 21:22:27 +0000

    COBJAPI: 2frames.cob added.

--- tools/cobjapi/SWING/examples_simple/2frames.cob
+++ tools/cobjapi/SWING/examples_simple/2frames.cob
@@ -1,23 +1,23 @@
 *>******************************************************************************
 *>  This file is part of cobjapi.
 *>
-*>  splitpane.cob is free software: you can redistribute it and/or 
+*>  2frames.cob is free software: you can redistribute it and/or 
 *>  modify it under the terms of the GNU Lesser General Public License as 
 *>  published by the Free Software Foundation, either version 3 of the License,
 *>  or (at your option) any later version.
 *>
-*>  splitpane.cob is distributed in the hope that it will be useful, 
+*>  2frames.cob is distributed in the hope that it will be useful, 
 *>  but WITHOUT ANY WARRANTY; without even the implied warranty of 
 *>  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 *>  See the GNU Lesser General Public License for more details.
 *>
 *>  You should have received a copy of the GNU Lesser General Public License 
-*>  along with splitpane.cob.
+*>  along with 2frames.cob.
 *>  If not, see <http://www.gnu.org/licenses/>.
 *>******************************************************************************
 
 *>******************************************************************************
-*> Program:      splitpane.cob
+*> Program:      2frames.cob
 *>
 *> Purpose:      Example GnuCOBOL program for JAPI
 *>
@@ -26,12 +26,12 @@
 *> Date-Written: 2018.03.13
 *>
 *> Tectonics:    Example for static link.
-*>               cobc -x -free splitpane.cob cobjapi.o \
-*>                                           japilib.o \
-*>                                           imageio.o \
-*>                                           fileselect.o
+*>               cobc -x -free 2frames.cob cobjapi.o \
+*>                                         japilib.o \
+*>                                         imageio.o \
+*>                                         fileselect.o
 *>
-*> Usage:        ./splitpane.exe
+*> Usage:        ./2frames.exe
 *>
 *>******************************************************************************
 *> Date       Name / Change description 
@@ -41,11 +41,11 @@
 *>------------------------------------------------------------------------------
 *> 2018.03.13 Laszlo Erdos: 
 *>            - GnuCOBOL support for JAPI added. 
-*>            - splitpane.c converted into splitpane.cob. 
+*>            - 2frames.c converted into 2frames.cob. 
 *>******************************************************************************
 
  IDENTIFICATION DIVISION.
- PROGRAM-ID. splitpane.
+ PROGRAM-ID. 2frames.
  AUTHOR.     Laszlo Erdos.
 
  ENVIRONMENT DIVISION.
@@ -54,15 +54,10 @@
     FUNCTION J-SETDEBUG
     FUNCTION J-START
     FUNCTION J-FRAME
-    FUNCTION J-SETBORDERLAYOUT
-    FUNCTION J-SPLITPANE
-    FUNCTION J-SETBORDERPOS
-    FUNCTION J-PANEL
-    FUNCTION J-SETCOLORBG
-    FUNCTION J-LABEL
-    FUNCTION J-SETSPLITPANELEFT
-    FUNCTION J-SETSPLITPANERIGHT
     FUNCTION J-SHOW
+    FUNCTION J-LOADIMAGE
+    FUNCTION J-SETSIZE
+    FUNCTION J-SETICON
     FUNCTION J-NEXTACTION
     FUNCTION J-QUIT
     FUNCTION ALL INTRINSIC.
@@ -70,30 +65,23 @@
  DATA DIVISION.
 
  WORKING-STORAGE SECTION.
- 01 J-HORIZONTAL                       BINARY-INT VALUE 0.
- 01 J-CENTER                           BINARY-INT VALUE 1.
-
- *> function return value 
+*> function return value 
  01 WS-RET                             BINARY-INT.
 
 *> GUI elements
- 01 WS-FRAME                           BINARY-INT.
- 01 WS-PANEL-1                         BINARY-INT.
- 01 WS-PANEL-2                         BINARY-INT.
- 01 WS-SP                              BINARY-INT.
- 01 WS-LABEL                           BINARY-INT.
+ 01 WS-FRAME-1                         BINARY-INT.
+ 01 WS-FRAME-2                         BINARY-INT.
+ 01 WS-ICON                            BINARY-INT.
  
 *> function args 
  01 WS-DEBUG-LEVEL                     BINARY-INT.
- 01 WS-POS                             BINARY-INT.
- 01 WS-R                               BINARY-INT.
- 01 WS-G                               BINARY-INT.
- 01 WS-B                               BINARY-INT.
-
+ 01 WS-WIDTH                           BINARY-INT.
+ 01 WS-HEIGHT                          BINARY-INT.
+ 
  PROCEDURE DIVISION.
 
 *>------------------------------------------------------------------------------
- MAIN-SPLITPANE SECTION.
+ MAIN-2FRAMES SECTION.
 *>------------------------------------------------------------------------------
 
 *>  MOVE 5 TO WS-DEBUG-LEVEL
@@ -106,37 +94,27 @@
        STOP RUN
     END-IF
 
+    MOVE J-LOADIMAGE("images/new.gif") TO WS-ICON  
+    
 *>  Generate GUI Objects    
-    MOVE J-FRAME("Split Pane Demo")  TO WS-FRAME  
-    MOVE J-SETBORDERLAYOUT(WS-FRAME) TO WS-RET
-    
-    MOVE 142 TO WS-POS
-    MOVE J-SPLITPANE(WS-FRAME, J-HORIZONTAL, WS-POS) TO WS-SP
-    MOVE J-SETBORDERPOS(WS-SP, J-CENTER) TO WS-RET
-    
-    MOVE J-PANEL(WS-SP) TO WS-PANEL-1
-    MOVE 255 TO WS-R
-    MOVE   0 TO WS-G
-    MOVE   0 TO WS-B
-	MOVE J-SETCOLORBG(WS-PANEL-1, WS-R, WS-G, WS-B)  TO WS-RET
-    MOVE J-LABEL(WS-PANEL-1, "Left Panel") TO WS-LABEL
-    
-    MOVE J-PANEL(WS-SP) TO WS-PANEL-2
-    MOVE 255 TO WS-R
-    MOVE 255 TO WS-G
-    MOVE   0 TO WS-B
-	MOVE J-SETCOLORBG(WS-PANEL-2, WS-R, WS-G, WS-B)  TO WS-RET
-    MOVE J-LABEL(WS-PANEL-2, "Right Panel") TO WS-LABEL
-    
-    MOVE J-SETSPLITPANELEFT(WS-SP, WS-PANEL-1)  TO WS-RET
-    MOVE J-SETSPLITPANERIGHT(WS-SP, WS-PANEL-2) TO WS-RET
-    
-    MOVE J-SHOW(WS-FRAME) TO WS-RET
+    MOVE J-FRAME("Frame Demo1") TO WS-FRAME-1  
+    MOVE 300 TO WS-WIDTH 
+    MOVE 400 TO WS-HEIGHT 
+    MOVE J-SETSIZE(WS-FRAME-1, WS-WIDTH, WS-HEIGHT) TO WS-RET
+    MOVE J-SHOW(WS-FRAME-1) TO WS-RET
+    MOVE J-SETICON(WS-FRAME-1, WS-ICON) TO WS-RET
+
+    MOVE J-FRAME("Frame Demo2") TO WS-FRAME-2  
+    MOVE 300 TO WS-WIDTH 
+    MOVE 200 TO WS-HEIGHT 
+    MOVE J-SETSIZE(WS-FRAME-2, WS-WIDTH, WS-HEIGHT) TO WS-RET
+    MOVE J-SHOW(WS-FRAME-2) TO WS-RET
+    MOVE J-SETICON(WS-FRAME-2, WS-ICON) TO WS-RET
 
 *>  Waiting for actions
     PERFORM FOREVER
        MOVE J-NEXTACTION() TO WS-RET
-       IF WS-RET = WS-FRAME
+       IF WS-RET = WS-FRAME-1
        THEN
           EXIT PERFORM
        END-IF
@@ -147,6 +125,6 @@
     STOP RUN
     
     .
- MAIN-SPLITPANE-EX.
+ MAIN-2FRAMES-EX.
     EXIT.
- END PROGRAM splitpane.
+ END PROGRAM 2frames.
