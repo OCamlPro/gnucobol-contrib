1df8796edce210361c7e689eda493bb515a65f98
László Erdős <laszloerdos@users.sourceforge.net>
2022-12-14 12:24:09 +0000

    COBJAPI: make.sh and manifest.mf changed in src_java.

--- tools/cobjapi/SWING/src_java/make.sh
+++ tools/cobjapi/SWING/src_java/make.sh
@@ -8,6 +8,7 @@ echo "--> Creating tmp directory and copying files"
 mkdir tmpdir
 cd tmpdir/
 cp -r ../src/ .
+cp ../manifest.mf src/de/.
 
 echo "--> Compiling Japi2 Kernel"
 javac $(find . -name *.java)
@@ -16,21 +17,12 @@ echo "--> Cleanup of java resources"
 find . -name "*.java" -type f -delete
 
 echo "--> Packaging into JAR file"
-mkdir META-INF
-jar -cf japi2.jar .
 
-echo "--> Generating manifest"
-echo "Manifest-Version: 1.0\nMain-Class: de.japi.Japi2\n" > META-INF/MANIFEST.MF
-(7z a japi2.jar META-INF/) > log.txt
+cd src
+jar -cvmf de/manifest.mf japi2.jar .
 
-echo "--> Compressing JAR file"
-pack200 --repack --effort=9 --segment-limit=-1 --modification-time=latest --strip-debug japi2-compressed.jar japi2.jar 
-
-echo "     Original size....: $(wc -c < japi2.jar) bytes"
-echo "     Compressed size..: $(wc -c < japi2-compressed.jar) bytes"
-
-cd ../
-cp tmpdir/japi2-compressed.jar japi2.jar
+cd ../../
+cp tmpdir/src/japi2.jar japi2.jar
 rm -rf tmpdir/
 
 echo " "
